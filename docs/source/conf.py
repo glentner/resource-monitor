e337434d239ba5492cd2636f580dec65aa19bd62
Geoffrey Lentner <glentner@purdue.edu>
2021-12-17 00:37:20 -0500

    Add dynamic load of __version__ in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,19 +10,23 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import datetime
+sys.path.insert(0, os.path.abspath('../..'))
 
+import monitor  # noqa
 
 # -- Project information -----------------------------------------------------
 
+year = datetime.datetime.now().year
 project = 'Resource Monitor'
-copyright = '2019 Geoffrey Lentner'
-author = 'Geoffrey Lentner'
+copyright = f'2019 Geoffrey Lentner'
+author = 'Geoffrey Lentner <glentner@purdue.edu>'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.2'
+release = monitor.__version__
+version = monitor.__version__
 
 
 # -- General configuration ---------------------------------------------------
